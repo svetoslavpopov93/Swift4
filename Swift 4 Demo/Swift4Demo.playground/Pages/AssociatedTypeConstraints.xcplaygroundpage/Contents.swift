a7f8c49868def85c6524ce033773089d3774e0e2
Miroslav Danazhiev <miroslav.danazhiev@mentormate.com>
2017-06-26 17:44:14 +0300

    associate types fixes

--- Swift 4 Demo/Swift4Demo.playground/Pages/AssociatedTypeConstraints.xcplaygroundpage/Contents.swift
+++ Swift 4 Demo/Swift4Demo.playground/Pages/AssociatedTypeConstraints.xcplaygroundpage/Contents.swift
@@ -6,65 +6,69 @@
 
 //: ### In Swift 4
 import Foundation
+
 protocol SequenceABC {
-        associatedtype Element
-        associatedtype SubSequence : Sequence where SubSequence.Iterator.Element == Iterator.Element
-                                             // ^----- This was not allowed in the previous versions
-    }
+    
+    associatedtype Iterator : IteratorProtocol
+    associatedtype SubSequence : Sequence where SubSequence.Iterator.Element == Iterator.Element
+    // ^----- This was not allowed in the previous versions
+}
 
 //: ## Change 'filter' to return an associated type [SE-0174]
 //: ### In Swift 3:
 
 
-    let drinks = ["Ariana Radler":2.1, "Coca-Cola":1.1, "Cappy":1.5]
-    var overpricedDrinks = drinks.filter { (value) -> Bool in
-        return value.value > 1.5
-    }
+let drinks = ["Ariana Radler":2.1, "Coca-Cola":1.1, "Cappy":1.5]
+var overpricedDrinks = drinks.filter { (value) -> Bool in
+    return value.value > 1.5
+}
 
-    type(of: overpricedDrinks) // overpricedDrinks is array of tuples
+type(of: overpricedDrinks) // overpricedDrinks is array of tuples
 
 //: ### In Swift 4:
 // The new filter implementation
-    protocol SequenceABCD {
-        associatedtype Filtered
-        
-        func filter(_ isIncluded: (Iterator.Element) throws -> Bool) rethrows -> Filtered // filter(_:) will now return the associatedtype
-    }
+
+protocol SequenceABCD {
+    
+    associatedtype Filtered
+    associatedtype Iterator : IteratorProtocol
+    
+    func filter(_ isIncluded: (Iterator.Element) throws -> Bool) rethrows -> Filtered // filter(_:) will now return the associatedtype
+}
 
 // The default filter(_:) implementation of RangeReplaceableCollection
-    extension RangeReplaceableCollection {
-        func filter(_ isIncluded: (Iterator.Element) throws -> Bool) rethrows -> Self {
-            var result = Self()
-            for element in self {
-                if try isIncluded(element) {
-                    result.append(element)
-                }
+extension RangeReplaceableCollection {
+    func filter(_ isIncluded: (Iterator.Element) throws -> Bool) rethrows -> Self {
+        var result = Self()
+        for element in self {
+            if try isIncluded(element) {
+                result.append(element)
             }
-            return result
         }
+        return result
     }
+}
 
 
 //: ## Support recursive constraints on associated types [SE-0157]
-    // This will compile
-    protocol Recordable { }
-
-    protocol Watchable {
-        associatedtype WatchableItem: Recordable
-    }
-
-    // This will not compile
-    protocol SequenceABCDE {
-        associatedtype SubSequence
-    
-        func dropFirst(_ n: Int) -> Self.SubSequence
-    }
-
-    struct SequenceOfInts : SequenceABCDE {
-        func dropFirst(_ n: Int) -> SequenceABCDE {
-            return A()              // ^------ This is not allowed in the prevoius version
-        }
-    }
-
-
+//protocol Testable {
+//    associatedtype SubSequence
+//    associatedtype Iterator : IteratorProtocol
+//    func dropFirst(_ n: Int) -> Self.SubSequence
+//}
+//
+//struct SequenceOfInts : Testable {
+//
+//    func dropFirst(_ n: Int) -> SequenceOfInts.SubSequence {
+//
+//        let sub: SimpleSubSequence<Int> = SimpleSubSequence()
+//        return sub
+//    }
+//}
+//
+//struct SimpleSubSequence<Element> : Testable {
+//
+//    typealias SubSequence = SimpleSubSequence<Element>
+//
+//}
 //: [< Previous](@previous)   [Next >](@next)
