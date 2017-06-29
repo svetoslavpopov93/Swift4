0cc12a190a1e3b836de4b35336d02bae7af75966
Miroslav Danazhiev <miroslav.danazhiev@mentormate.com>
2017-06-29 17:52:57 +0300

    Generics

--- Swift 4 Demo/Swift4Demo.playground/Pages/GenericSubscripts.xcplaygroundpage/Contents.swift
+++ Swift 4 Demo/Swift4Demo.playground/Pages/GenericSubscripts.xcplaygroundpage/Contents.swift
@@ -1,8 +1,47 @@
 //: [<< Table Of Contents](TableOfContents)
 //:
 //: # Generic subscripts
+/*
+ Courtesy of [SE-0148], subscripts can now have generic arguments and/or return types.
+  https://github.com/apple/swift-evolution/blob/master/proposals/0148-generic-subscripts.md "Swift Evolution Proposal SE-0148: Generic Subscripts"
+ */
 //:------------
+struct JSON {
+    fileprivate var storage: [String:Any]
+    
+    init(dictionary: [String:Any]) {
+        self.storage = dictionary
+    }
+    
+    subscript<T>(key: String) -> T? {
+        return storage[key] as? T
+    }
+}
 
+let json = JSON(dictionary: [
+    "name": "Berlin",
+    "country": "de",
+    "population": 3_500_500
+    ])
+
+// No need to use as? Int
+let population: Int? = json["population"]
+
+/*:
+ Another example: a subscript on `Collection` that takes a generic sequence of indices and returns an array of the values at these indices:
+ */
+extension Collection {
+    subscript<Indices: Sequence>(indices indices: Indices) -> [Element] where Indices.Element == Index {
+        var result: [Element] = []
+        for index in indices {
+            result.append(self[index])
+        }
+        return result
+    }
+}
+
+let words = "Lorem ipsum dolor sit amet".split(separator: " ")
+words[indices: [1,2]]
 //:------------
 //: [< Previous](@previous)   [Next >](@next)
 
