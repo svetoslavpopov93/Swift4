bd38426cc7332d75d10ad95f3a7d3c7945d9747a
Miroslav Danazhiev <miroslav.danazhiev@mentormate.com>
2017-06-29 17:58:42 +0300

    bridging and strings

--- Swift 4 Demo/Swift4Demo.playground/Pages/NSNumberBridging.xcplaygroundpage/Contents.swift
+++ Swift 4 Demo/Swift4Demo.playground/Pages/NSNumberBridging.xcplaygroundpage/Contents.swift
@@ -1,7 +1,15 @@
 //: [<< Table Of Contents](TableOfContents)
 //:
 //: # NSNumber bridging
+/*
+[SE-0170] fixes some dangerous behavior when bridging between native Swift number types and `NSNumber`.
+https://github.com/apple/swift-evolution/blob/master/proposals/0170-nsnumber_bridge.md "Swift Evolution Proposal SE-0170: NSNumber bridging and Numeric types"
+*/
 //:------------
+import Foundation
+
+let n = NSNumber(value: UInt32(543))
+let v = n as? Int8 // nil in Swift 4. This would be 31 in Swift 3 (try it!).
 
 //:------------
 //: [< Previous](@previous)   [Next >](@next)
